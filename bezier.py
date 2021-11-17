f9f0bbcd6912fd5f9de64b2f1c4468c274747c32
Brankonymous <brankogrbic01@gmail.com>
2021-11-17 13:45:21 +0100

    fixed

--- bezier.py
+++ bezier.py
@@ -4,18 +4,19 @@ import numpy as np
 
 class DotToLine(Scene):
     def construct(self):
-        # Hold na 2s
+        # Hold na 1s
         self.wait()
 
-        # Konstrukcija tacaka i linije
+        # Konstrukcija tacaka
         dot1 = Dot()
         dot2 = Dot()
-        print(dot1.width, dot2.width)
 
+        # Podesavanje sredisnje tacke
         mid_dot = Dot()
         mid_dot.set_fill(BLACK)
         mid_dot.set_stroke(BLUE, width=4)
 
+        # Pokretanje animacija
         self.play(
                 Create(dot1),
                 Create(dot2)
@@ -28,8 +29,10 @@ class DotToLine(Scene):
                 dot2.animate.shift(3 * RIGHT + 2 * UP)
             )
 
+        # Definisanje linije i mid_line koja prati sredisnju tacku
         line = Line(start=dot1.get_center(), end=dot2.get_center(), color=GREY)
         mid_line = Line(start=dot1.get_center(), end=mid_dot.get_center(), color=GREY)
+
         self.play(
                 Create(line),
                 Create(mid_dot),
@@ -37,11 +40,10 @@ class DotToLine(Scene):
 
         self.wait()
 
-        # 2 dots
+        # P_MID = (1 - t) * P[0] + t * P[1]
         t = 0.5
-        mid_dot_brace = BraceBetweenPoints(dot1.get_center(), mid_dot.get_center(), [-2., 3., 0])
-        mid_dot_brace.add_updater(lambda z: z.become(BraceBetweenPoints(dot1.get_center(), mid_dot.get_center(), [-2., 3., 0])))
 
+        # Label koji prati sredisnju tacku
         mid_text, mid_number = label = VGroup(
             Text("t = "),
             DecimalNumber(
@@ -50,18 +52,21 @@ class DotToLine(Scene):
             )
         )
         label.arrange(RIGHT)
+        label.scale(0.8)
 
         label.add_updater(
-                lambda m: m.next_to(mid_dot_brace, UP)
+                lambda m: m.next_to(mid_dot, UP + LEFT)
             )
 
+        # Updater sredisnje tacke
         mid_number.add_updater(lambda m: m.set_value(
                 (mid_dot.get_center()[0] - dot1.get_center()[0]) / (dot2.get_center()[0] - dot1.get_center()[0])
             ))
 
-        self.play(Create(mid_dot_brace), Create(label))
+        self.play(Create(label))
         self.wait()
 
+        # Animacije kretanja sredisnje tacke
         mid_dot_x = ValueTracker(0)
         mid_dot_y = ValueTracker(0)
         mid_dot.add_updater(lambda z: z.set_x(mid_dot_x.get_value()))
@@ -84,10 +89,12 @@ class DotToLine(Scene):
         )
         self.wait()
 
-        self.remove(mid_dot_brace)
-        self.remove(label)
+        self.play(
+            Uncreate(label),
+            Uncreate(mid_dot)
+            )
 
-        self.wait(2)
+        self.wait()
 
 '''
 
